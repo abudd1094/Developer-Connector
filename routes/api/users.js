26476f4a967d768686519eef2fb47584de89329e
Alec Budd <abudd1094@gmail.com>
2019-04-22 15:51:33 +0200

    initial commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+// Load User Model
+const User = require('../../models/User');
+
 // @route   GET api/users/test
-// @desc    Tests users route
+// @desc    Test users route
 // @access  Public
 router.get('/test', (req, res) => res.json({msg: 'Users works'}));
+
+// @route   GET api/users/register
+// @desc    Register user
+// @access  Public
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email }) // search for a user with the requested username in the database
+});
 
 module.exports = router;
 
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users')
 const profile = require('./routes/api/profile')
 const posts = require('./routes/api/posts')
 
 const app = express();
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // DB Config
 const db = require('./config/keys').mongoURI; 
