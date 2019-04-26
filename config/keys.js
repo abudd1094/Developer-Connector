ddb3507bac1982fd73487b32331158334a0c4a55
Alec Budd <abudd1094@gmail.com>
2019-04-26 13:10:27 +0200

    profile route

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://abudd1094:Atlanta1994@devconnectordata-i91bb.mongodb.net/test?retryWrites=true',
+  mongoURI: 'mongodb+srv://abudd1094:Atlanta1994@devconnectordb-nbdu1.mongodb.net/test?retryWrites=true',
   secretOrKey: 'secret'
 }
 
